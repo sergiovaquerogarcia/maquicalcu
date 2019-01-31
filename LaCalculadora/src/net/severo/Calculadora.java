50e7dd450326048a3a0ee82b679b47b79c42b79d
Profe <root@usuarioEntDes>
2019-01-31 21:27:36 +0100

    Algún fallo habrá que resolver

--- LaCalculadora/src/net/severo/Calculadora.java
+++ LaCalculadora/src/net/severo/Calculadora.java
@@ -1,7 +1,9 @@
 package net.severo;
 
 import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -14,30 +16,44 @@ public class Calculadora extends JFrame {
 		super("Calculadora");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(new BorderLayout());
+		LogicaDeCalculadora logic = new LogicaDeCalculadora();
 		JLabel display = new JLabel("0", SwingConstants.RIGHT);
+		ActionListener digitListener = new DetectaPulsaciones(logic, display);
+		ActionListener operatorListener = new DetectarOperaciones(logic, display);
 		JPanel buttonsPanel = new JPanel(new GridLayout(6, 3));
-		buttonsPanel.add(new JButton("AC"));
-		buttonsPanel.add(new JButton("+"));
+		agregarBoton(buttonsPanel,"AC",operatorListener);
+		agregarBoton(buttonsPanel,"+",operatorListener);
 		buttonsPanel.add(new JButton("-"));
 		buttonsPanel.add(new JButton("C"));
 		buttonsPanel.add(new JButton("*"));
 		buttonsPanel.add(new JButton("/"));
-		buttonsPanel.add(new JButton("7"));
+		agregarBoton(buttonsPanel,"7",digitListener);
 		buttonsPanel.add(new JButton("8"));
-		buttonsPanel.add(new JButton("9"));
+		agregarBoton(buttonsPanel,"9",digitListener);
 		buttonsPanel.add(new JButton("4"));
-		buttonsPanel.add(new JButton("5"));
+		agregarBoton(buttonsPanel,"5",digitListener);
 		buttonsPanel.add(new JButton("6"));
-		buttonsPanel.add(new JButton("1"));
+		agregarBoton(buttonsPanel,"1",digitListener);
 		buttonsPanel.add(new JButton("2"));
-		buttonsPanel.add(new JButton("3"));
+		agregarBoton(buttonsPanel,"3",digitListener);
 		buttonsPanel.add(new JButton("."));
 		buttonsPanel.add(new JButton("0"));
-		buttonsPanel.add(new JButton("="));
+		agregarBoton(buttonsPanel,"=",operatorListener);
 		getContentPane().add(display, BorderLayout.NORTH);
 		getContentPane().add(buttonsPanel, BorderLayout.CENTER);
 		pack();
 		setVisible(true);
+	}
+
+	private void agregarBoton(Container ventanaPrincipal, String texto, ActionListener efecto) {
+		JButton button = new JButton(texto);
+		if (efecto != null) {
+			button.addActionListener(efecto);
+		} else {
+			button.setEnabled(false);
+		}
+		ventanaPrincipal.add(button);
+
 	}
 
 	public static void main(String[] args) {
